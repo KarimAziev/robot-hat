30a5d9b76ad2c05586fa9ee4305cb5dde1c0620a
KarimAziev <karim.aziiev@gmail.com>
2024-12-20 14:49:18 +0200

    feat: Add list parsing and conversion methods to FileDB class

--- robot_hat/filedb.py
+++ robot_hat/filedb.py
@@ -1,9 +1,8 @@
+import logging
 import os
 from dataclasses import dataclass
 from pathlib import Path
-from typing import List, TypeVar, Union, Dict
-
-import logging
+from typing import Callable, Dict, List, Optional, TypeVar, Union
 
 logger = logging.getLogger(__name__)
 
@@ -85,6 +84,102 @@ class FileDB(object):
             if key.strip() == name:
                 return val.replace(" ", "").strip()
         return default_value
+
+    def get_value_with(self, name: str, fn: Callable[[str], T]) -> Optional[T]:
+        value = self.get(name, default_value=None)
+        if value:
+            return fn(value)
+
+    @staticmethod
+    def split_list_str(value: str) -> List[str]:
+        """
+        Splits a string representation of a list into individual string elements.
+
+        The input string is expected to be a comma-separated list of elements
+        enclosed in square brackets (e.g., "[a, b, c]"). Leading and trailing
+        whitespace around elements is removed. Empty elements are ignored.
+
+        Args:
+            value (str): The string representation of a list to parse.
+
+        Returns:
+            List[str]: A list of strings extracted from the input string.
+
+        Example:
+            input = "[ a, 1.3 , c ]"
+            output = ["a", "1.3", "c"]
+
+            input = "[]"
+            output = []
+        """
+        return [i.strip() for i in value.strip("[]").split(",") if i.strip()]
+
+    def get_list_value_with(self, name: str, fn: Callable[[str], T]) -> List[T]:
+        """
+        Retrieves a list value associated with a given key in the database and processes
+        each element using a provided conversion function.
+
+        This method retrieves the value for the specified key, parses it into a list
+        of strings, and applies the provided conversion function (`fn`) to each element
+        in the list. The converted list is returned.
+
+        Args:
+            name (str): The key to look up in the database.
+            fn: A function that maps a string element to the desired type
+                (e.g., `int` for integers, `float` for floating-point numbers).
+
+        Returns:
+            A list of elements converted using the provided function `fn`.
+
+        Example:
+            Assuming the database contains the key-value pair `"key" = "[1, 2, 3]"`:
+                result = get_list_value_with("key", int)  # result is [1, 2, 3]
+
+            For the input with `"key" = "[1.1, 2.2, 3.3]"`:
+                result = get_list_value_with("key", float)  # result is [1.1, 2.2, 3.3]
+        """
+        elems = self.split_list_str(self.get(name, default_value="[]"))
+
+        return [fn(v) for v in elems]
+
+    def read_int_list(self, name: str, default_value: List[int] = []) -> List[int]:
+        """
+        Reads and returns a list of integers from the database for the given key.
+
+        The value associated with the key is expected to be a comma-separated list
+        of integers enclosed in square brackets (e.g., "[1,2,3]"). If the key is
+        missing or the value cannot be parsed, the provided default value is returned.
+
+        Args:
+            name: The key to look up in the database.
+            default_value: The default list of integers to return if the key is missing or invalid.
+
+        Returns:
+            A list of integers parsed from the database value, or the default value if parsing fails.
+        """
+        value = self.get_list_value_with(name, int)
+        return value or default_value
+
+    def read_float_list(
+        self, name: str, default_value: List[float] = []
+    ) -> List[float]:
+        """
+        Reads and returns a list of floating-point numbers from the database for the given key.
+
+        The value associated with the key is expected to be a comma-separated list
+        of floating-point numbers enclosed in square brackets (e.g., "[1.1,2.2,3.3]").
+        If the key is missing or the value cannot be parsed, the provided default value is returned.
+
+        Args:
+            name: The key to look up in the database.
+            default_value: The default list of floats to return
+                if the key is missing or invalid. Defaults to an empty list ([]).
+
+        Returns:
+            A list of floating-point numbers parsed from the database value,
+            or the default value if parsing fails.
+        """
+        return self.get_list_value_with(name, float) or default_value
 
     def parse_file(self) -> List[str]:
         """
