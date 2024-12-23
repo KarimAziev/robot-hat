cda20511edaa867056ac303c01cd3057b5ecc637
KarimAziev <karim.aziiev@gmail.com>
2024-12-23 10:44:42 +0200

    docs: Improve ADXL345 class documentation and comments

--- robot_hat/accelerometer.py
+++ robot_hat/accelerometer.py
@@ -9,29 +9,12 @@ from .i2c import I2C
 
 class ADXL345(I2C):
     """
-    The ADXL345 is a small, thin, low power, 3-axis accelerometer with high resolution (13-bit) measurement at up to ±16g.
+    This class provides interface for an ADXL345 accelerometer sensor using I2C.
 
-    Digital output data is formatted as 16-bit two's complement and is accessible through either:
-    - a SPI (3- or 4-wire)
-    - I2C digital interface.
-
-    ### Attributes:
-        - `X` (int): Represents the X-axis.
-        - `Y` (int): Represents the Y-axis.
-        - `Z` (int): Represents the Z-axis.
-        - `ADDR` (int): Default I2C address for the ADXL345.
-        - `_REG_DATA_X` (int): Register address for X-axis data.
-        - `_REG_DATA_Y` (int): Register address for Y-axis data.
-        - `_REG_DATA_Z` (int): Register address for Z-axis data.
-        - `_REG_POWER_CTL` (int): Register address for power control.
-        - `_AXISES` (list): List of register addresses for X, Y, Z axis data.
-
-    ### Methods:
-        - `__init__(self, *args, address: int = ADDR, bus: int = 1, **kwargs)`: Initializes the ADXL345 sensor.
-        - `read(self, axis: Optional[int] = None) -> Union[float, List[float]]`: Reads the specified axis data or all axis data.
-        - `_read(self, axis: int) -> float`: Reads data from a specified axis register.
+    The ADXL345 is a small 3-axis accelerometer with high resolution (13-bit) measurement at up to ±16g.
 
     ### Example usage:
+
     ```python
     adxl = ADXL345()
     x, y, z = adxl.read()  # Reads all axis data
@@ -40,24 +23,40 @@ class ADXL345(I2C):
     """
 
     X = 0
-    """X"""
+    """The X-axis."""
+
     Y = 1
-    """Y"""
+    """The Y-axis"""
+
     Z = 2
-    """Z"""
+    """The Z-axis"""
+
     ADDR = 0x53
-    _REG_DATA_X = 0x32  # X-axis data 0 (6 bytes for X/Y/Z)
-    _REG_DATA_Y = 0x34  # Y-axis data 0 (6 bytes for X/Y/Z)
-    _REG_DATA_Z = 0x36  # Z-axis data 0 (6 bytes for X/Y/Z)
-    _REG_POWER_CTL = 0x2D  # Power-saving features control
+    """Default I2C address for the ADXL345 """
+
+    _REG_DATA_X = 0x32
+    """Register address for X-axis data."""
+
+    _REG_DATA_Y = 0x34
+    """Register address for Y-axis data."""
+
+    _REG_DATA_Z = 0x36
+    """Register address for Z-axis data."""
+
+    _REG_POWER_CTL = 0x2D
+    """Register address for power control."""
+
     _AXISES = [_REG_DATA_X, _REG_DATA_Y, _REG_DATA_Z]
+    """List of register addresses for X, Y, Z axis data."""
 
     def __init__(self, *args, address: int = ADDR, bus: int = 1, **kwargs):
         """
-        Initialize ADXL345
+        Initializes the ADXL345 sensor.
 
-        :param address: address of the ADXL345
-        :type address: int
+        Args:
+
+        - address: The address of I2C device.
+        - bus: I2C bus number.
         """
         super().__init__(address=address, bus=bus, *args, **kwargs)
         self.address = address
@@ -66,12 +65,10 @@ class ADXL345(I2C):
         self, axis: Optional[int] = None
     ) -> Union[float, List[Union[float, None]], None]:
         """
-        Read an axis from ADXL345
+        Reads the specified axis data or all axis data.
 
-        :param axis: read value(g) of an axis, ADXL345.X, ADXL345.Y or ADXL345.Z, None for all axis
-        :type axis: int
-        :return: value of the axis, or list of all axis
-        :rtype: float/list
+        Args:
+        axis: The axis to read (X=0, Y=1, Z=2). If None, data for all axes will be read.
         """
         if axis is None:
             return [self._read(i) for i in range(3)]
@@ -79,6 +76,9 @@ class ADXL345(I2C):
             return self._read(axis)
 
     def _read(self, axis: int) -> Union[float, None]:
+        """
+        Reads data from a specified axis register.
+        """
         raw_2 = 0
         result = super().read()
         data = (0x08 << 8) + self._REG_POWER_CTL
