5117e9ee710ad59e0f887896c24afcb5a7e5ad07
KarimAziev <karim.aziiev@gmail.com>
2024-12-24 16:47:40 +0200

    refactor: Allow int type for motor pin configuration

--- robot_hat/motor/config.py
+++ robot_hat/motor/config.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Union
 
 
 class MotorConfig:
@@ -11,8 +11,8 @@ class MotorConfig:
 
     def __init__(
         self,
-        dir_pin: str,
-        pwm_pin: str,
+        dir_pin: Union[str, int],
+        pwm_pin: Union[str, int],
         calibration_direction: int = 1,
         calibration_speed_offset: float = 0,
         max_speed: int = 100,
@@ -24,14 +24,14 @@ class MotorConfig:
         Initialize the configuration.
 
         Args:
-            dir_pin (str): Pin controlling motor direction.
-            pwm_pin (str): Pin controlling motor speed (PWM).
-            calibration_direction (Direction): Initial motor direction calibration (+1/-1).
-            calibration_speed_offset (float): Initial motor speed calibration offset.
-            max_speed (int): Maximum allowable speed for the motor.
-            period (int): PWM period for speed control.
-            prescaler (int): PWM prescaler for speed control.
-            name (str): Optional human-readable name for the motor.
+            dir_pin: Number of Pin controlling motor direction. Either the name as string, or number.
+            pwm_pin: PWM channel number or name.
+            calibration_direction: Initial motor direction calibration (+1/-1).
+            calibration_speed_offset: Initial motor speed calibration offset.
+            max_speed: Maximum allowable speed for the motor.
+            period: PWM period for speed control.
+            prescaler: PWM prescaler for speed control.
+            name: human-readable name for the motor.
         """
         self.dir_pin = dir_pin
         self.pwm_pin = pwm_pin
