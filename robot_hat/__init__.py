f9fdf1e53e6093a43bdfd68bb0e594ac6ad26cf3
KarimAziev <karim.aziiev@gmail.com>
2025-09-18 09:57:20 +0300

    refactor: update imports and exceptions in robot_hat module

--- robot_hat/__init__.py
+++ robot_hat/__init__.py
@@ -1,8 +1,10 @@
 from robot_hat.data_types.config.motor import MotorConfigType, MotorDirection
-from robot_hat.drivers.adc.sunfounder_adc import ADC
 from robot_hat.exceptions import (
     ADCAddressNotFound,
+    DevicePinFactoryError,
     FileDBValidationError,
+    GrayscaleTypeError,
+    IMUInitializationError,
     InvalidCalibrationModeError,
     InvalidChannel,
     InvalidChannelName,
@@ -14,17 +16,19 @@ from robot_hat.exceptions import (
     InvalidPinNumber,
     InvalidPinPull,
     InvalidServoAngle,
+    MotorFactoryError,
+    MotorValidationError,
     UltrasonicEchoPinError,
+    UnsupportedMotorConfigError,
 )
 from robot_hat.factories.motor_factory import MotorFactory
 from robot_hat.filedb import FileDB
 from robot_hat.i2c.i2c_manager import I2C
 from robot_hat.mock.ultrasonic import Ultrasonic as UltrasonicMock
-from robot_hat.motor.i2c_dc_motor import I2CDCMotor as I2CDCMotor
+from robot_hat.motor.i2c_dc_motor import I2CDCMotor
 from robot_hat.music import Music
 from robot_hat.pin import Pin
 from robot_hat.sensors.ultrasonic.HC_SR04 import Ultrasonic
-from robot_hat.services.battery.sunfounder_battery import Battery
 from robot_hat.services.motor_service import MotorService
 from robot_hat.services.servo_service import ServoCalibrationMode, ServoService
 from robot_hat.servos.servo import Servo
@@ -32,9 +36,7 @@ from robot_hat.utils import compose, constrain, is_raspberry_pi, mapping
 from robot_hat.version import version
 
 __all__ = [
-    "ADC",
     "FileDB",
-    "Battery",
     "I2C",
     "Ultrasonic",
     "Music",
@@ -46,7 +48,21 @@ __all__ = [
     "ServoCalibrationMode",
     "ServoService",
     "UltrasonicMock",
+    "MotorConfigType",
+    "MotorDirection",
+    "compose",
+    "constrain",
+    "mapping",
+    "is_raspberry_pi",
+    "ADCAddressNotFound",
+    "DevicePinFactoryError",
     "FileDBValidationError",
+    "GrayscaleTypeError",
+    "IMUInitializationError",
+    "InvalidCalibrationModeError",
+    "InvalidChannel",
+    "InvalidChannelName",
+    "InvalidChannelNumber",
     "InvalidPin",
     "InvalidPinInterruptTrigger",
     "InvalidPinMode",
@@ -54,18 +70,9 @@ __all__ = [
     "InvalidPinNumber",
     "InvalidPinPull",
     "InvalidServoAngle",
-    "InvalidChannel",
-    "InvalidChannelName",
-    "InvalidChannelNumber",
-    "InvalidCalibrationModeError",
-    "MotorConfigType",
-    "MotorDirection",
+    "MotorFactoryError",
+    "MotorValidationError",
     "UltrasonicEchoPinError",
-    "compose",
-    "constrain",
-    "mapping",
-    "is_raspberry_pi",
-    "ADCAddressNotFound",
-    "ADCAddressNotFound",
+    "UnsupportedMotorConfigError",
     "version",
 ]
